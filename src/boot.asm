41c738c3295569c72a9eed6f83410a3843f82dfd
ChanSoo Shin <csshin9928@gmail.com>
2026-02-11 16:26:03 +0900

    feat: implement basic 32bit safe mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,9 @@
 [BITS 16]
 [ORG 0x7c00]
 
+CODE_OFFSET equ 0x8
+DATA_OFFSET equ 0x10
+
 start:
   cli
   mov ax, 0x00
@@ -9,24 +12,59 @@ start:
   mov ss, ax
   mov sp, 0x7c00
   sti
-  mov si, msg
 
 
-print:
-  lodsb
-  cmp al, 0
-  je done
-  mov ah, 0X0E
-  int 0X10
-  jmp print
-
-
-done:
+load_PM:
   cli
-  hlt
+  lgdt[gdt_descriptor]
+  mov eax, cr0
+  or al, 1
+  mov cr0, eax
+  jmp CODE_OFFSET:PModeMain 
 
 
-msg: db 'Hello World!', 0
+gdt_start:
+  dd 0x0
+  db 0x0
+
+  dw 0xFFFF
+  dw 0x0000
+  dw 0x00
+  db 10011010b
+  db 11001111b
+  db 0x00
+
+  dw 0xFFFF
+  dw 0x0000
+  dw 0x00
+  db 10010010b
+  db 11001111b
+  db 0x00
+
+
+gdt_end:
+
+
+gdt_descriptor:
+  dw gdt_end - gdt_start - 1
+
+
+[BITS 32]
+PModeMain:
+  mov ax, DATA_OFFSET
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov ss, ax
+  mov gs, ax
+  mov ebp, 0x9C00
+  mov esp, ebp
+
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+
+  jmp $
 
 
 times 510 - ($ - $$) db 0
